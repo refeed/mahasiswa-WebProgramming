14bf8e6a250f79111106a81ebef889862d4bed59
Rafid Aslam (refeed) <rafidteam@gmail.com>
2021-10-28 16:50:22 +0700

    Make navbar as partials

--- resources/views/layouts/buku_base.blade.php
+++ resources/views/layouts/buku_base.blade.php
@@ -39,32 +39,7 @@
 
 <body>
 
-    <nav class="navbar navbar-expand-md navbar-dark bg-dark mb-4">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">Buku</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarCollapse">
-                <ul class="navbar-nav me-auto mb-2 mb-md-0">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="{{ url('/buku') }}">Home</a>
-                    </li>
-                </ul>
-                @if (Route::has('login'))
-                    @auth
-                    <a href="{{ url('/home') }}" class="btn btn-outline-success">Home</a>
-                    @else
-                    <a href="{{ route('login') }}" class="btn btn-outline-success">Login</a>
-                        @if (Route::has('register'))
-                        <a href="{{ route('register') }}" class="btn btn-outline-success">Register</a>
-                        @endif
-                    @endauth
-                @endif
-
-            </div>
-        </div>
-    </nav>
+    @include('partials.navbar')
 
     <main class="container">
         @yield('content')
---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,47 @@
+
+<nav class="navbar navbar-expand-md navbar-dark bg-dark mb-4">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">Buku</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+            <ul class="navbar-nav me-auto mb-2 mb-md-0">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="{{ url('/buku') }}">Home</a>
+                </li>
+            </ul>
+            <ul class="navbar-nav ml-auto">
+                <!-- Authentication Links -->
+                @guest
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                </li>
+                @if (Route::has('register'))
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                </li>
+                @endif
+                @else
+                <li class="nav-item dropdown">
+                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                        {{ Auth::user()->name }}
+                    </a>
+
+                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                            {{ __('Logout') }}
+                        </a>
+
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                            @csrf
+                        </form>
+                    </div>
+                </li>
+                @endguest
+            </ul>
+
+        </div>
+    </div>
+</nav>
